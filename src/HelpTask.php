871e48a461aa6c9ecd199cb52174edbe0f0285be
Mateusz Aniolek <matty201@gmail.com>
2016-07-01 16:31:35 +0200

    task list all available tasks

--- src/HelpTask.php
+++ src/HelpTask.php
@@ -28,24 +28,74 @@ class HelpTask extends \Vegas\Cli\TaskAbstract
 
     public function listAction()
     {
-        $declaredClasses = get_declared_classes();
-        $taskClasses = preg_grep('/^.*Task\\\\.*Task$/x', $declaredClasses);
+        $declaredClasses = $this->getAvailableTasks();
 
         $taskInfo = "\n\t- App\\Task\n\t- (module name)\\Task\n\t- Vegas\\Task";
 
-        if (count($taskClasses)) {
+        if (count($declaredClasses)) {
             $this->putSuccess('Following tasks found:');
         } else {
             $this->putText('Not found any tasks with these namespaces: ' . $taskInfo);
         }
 
-        foreach ($taskClasses as $class) {
+        foreach ($declaredClasses as $class) {
             $this->putText(' - ' . $class);
         }
 
-        if (count($taskClasses)) {
+        if (count($declaredClasses)) {
             $this->putText("If there's a missing tasks, make sure they have one of the following namespaces:" . $taskInfo);
         }
 
+    }
+
+    /**
+     * @return array
+     */
+    protected function getAvailableTasks()
+    {
+        $response = [];
+        $directories = [];
+
+        $directories[] = [
+            'path' => APP_ROOT . '/app/tasks',
+            'namespace' => '\App\Task'
+        ];
+
+        $config = $this->getDI()->get('config');
+        $modules = $config->application->modules;
+
+        foreach ($modules as $module) {
+            $modulePath = APP_ROOT . DIRECTORY_SEPARATOR . $config->application->modulesDirectory .
+                DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . 'Task';
+            $directories[] = [
+                'path' => $modulePath,
+                'namespace' => '\\' . $module .'\Task'
+            ];
+        }
+
+        foreach ($directories as $directory) {
+
+            if (!file_exists($directory['path'])) {
+                continue;
+            }
+
+            $directoryIterator = new \DirectoryIterator($directory['path']);
+            foreach ($directoryIterator as $fileInfo) {
+
+                if ($fileInfo->isDot() || $fileInfo->isDir()) {
+                    continue;
+                }
+
+                $taskNamespace = $directory['namespace'] . '\\' . $fileInfo->getBasename('.php');
+
+                $class = new \ReflectionClass($taskNamespace);
+                if ($class->isSubclassOf('\Vegas\Cli\TaskAbstract')) {
+                    $response[] = $class->getName();
+                }
+
+            }
+        }
+
+        return $response;
     }
 }
